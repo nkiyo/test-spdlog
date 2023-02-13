22146b4923f94d8a3fa2cce2c4d83fdb1c80b6a1
nkiyo <nakahara.kiyoshi@gmail.com>
2023-02-14 08:32:55 +0900

    set log format

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,17 @@
 #include <string>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
 #include "spdlog/spdlog.h"
 
 int main () {
-    // TODO set log format
+    // set log format
     // => https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
-    //spdlog::set_pattern("");
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_pattern("%Y-%m-%d %H:%M:%S.%e %@ [%l] %v");
+    spdlog::set_level(spdlog::level::debug); // define SPDLOG_ACTIVE_LEVEL と両方必要
     int n = 1;
     std::string s = "hoge";
-    spdlog::error("error log のテスト");
-    spdlog::warn("warn log のテスト");
-    spdlog::info("info log のテスト {} {}", n, s.c_str());
-    spdlog::debug("debug log のテスト"); // default 設定だと出ない
+    SPDLOG_ERROR("error log のテスト");
+    SPDLOG_WARN("warn log のテスト");
+    SPDLOG_INFO("info log のテスト {} {}", n, s.c_str());
+    SPDLOG_DEBUG("debug log のテスト"); // default 設定だと出ない
 }
 
